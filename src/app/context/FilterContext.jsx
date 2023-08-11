fc606146ea3286bea5da0aaa270f140262cf5001
itsfarhankhan28 <famannat@gmail.com>
2023-08-11 23:44:42 +0530

    feat(FilterContext): created new context to filter movies

---
+++ src/app/context/FilterContext.jsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { BrowseMoviesContext } from "./AllMoviesContext";
+import reducer from '../reducer/FilterMoviesreducer'
+
+const FilterContext = createContext()
+
+const initialstate = {
+    filter_movies : [],
+    all_filter_movies : [],
+}
+
+const FilterProvider = ({children})=>{
+
+    const {allmovies} = BrowseMoviesContext()
+    console.log(allmovies)
+
+    const[state,dispatch] = useReducer(reducer,initialstate)
+
+    useEffect(()=>{
+        dispatch({type:"FILTER_MOVIES",payload:allmovies})
+    },[allmovies])
+
+    return <FilterContext.Provider value={{...state}}>
+        {children}
+    </FilterContext.Provider>
+}
+
+const FilterMoviesContext = ()=>{
+    return useContext(FilterContext)
+}
+
+export {FilterContext,FilterProvider,FilterMoviesContext}
