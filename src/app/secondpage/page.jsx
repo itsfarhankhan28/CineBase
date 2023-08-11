3f0311b56cbde095c8b2370986db4de12e9c263c
itsfarhankhan28 <famannat@gmail.com>
2023-08-11 23:46:21 +0530

    feat(page): added context to the all movies page

--- src/app/secondpage/page.jsx
+++ src/app/secondpage/page.jsx
@@ -7,28 +7,31 @@ import Navbar from '../component/Navigation/Navbar'
 import Sidebar from '../component/Navigation/Sidebar'
 import TopNavbar from '../component/Navigation/TopNavbar'
 // import { AllMoviesContext, BrowseMoviesContext } from '../context/AllMoviesContext'
+import { FilterProvider } from '../context/FilterContext'
 
 const page = () => {
 
   return (
-    <AppProvidertwo>
-      <div className='flex flex-row justify-between w-[100vw]'>
-        <div className='w-[15%]'>
-          <Navbar/>
-        </div>
-        <div className='w-[75%] h-auto bg-[#F7F8FF] overflow-y-visible pb-10'>
-          <div>
-            <TopNavbar/>
+      <AppProvidertwo>
+        <FilterProvider>
+          <div className='flex flex-row justify-between w-[100vw]'>
+            <div className='w-[15%]'>
+              <Navbar/>
+            </div>
+            <div className='w-[75%] h-auto bg-[#F7F8FF] overflow-y-visible pb-10'>
+              <div>
+                <TopNavbar/>
+              </div>
+              <div className='mt-10'>
+                <AllMovies/>
+              </div>
+            </div>
+            <div className='w-[10%]'>
+              <Sidebar/>
+            </div>
           </div>
-          <div className='mt-10'>
-            <AllMovies/>
-          </div>
-        </div>
-        <div className='w-[10%]'>
-          <Sidebar/>
-        </div>
-      </div>
-    </AppProvidertwo>
+        </FilterProvider>
+      </AppProvidertwo>
   )
 }
 
