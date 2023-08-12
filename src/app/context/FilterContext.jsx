6ea9dcafbb27311cd74d068395d7bf16143e2559
itsfarhankhan28 <famannat@gmail.com>
2023-08-12 16:44:13 +0530

    feat(FilterContext): added the search filter functionality

--- src/app/context/FilterContext.jsx
+++ src/app/context/FilterContext.jsx
@@ -3,12 +3,17 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
 import { BrowseMoviesContext } from "./AllMoviesContext";
 import reducer from '../reducer/FilterMoviesreducer'
+import { DisplaySettings } from "@mui/icons-material";
 
 const FilterContext = createContext()
 
 const initialstate = {
     filter_movies : [],
     all_filter_movies : [],
+    sorting_value:"Sci-Fi",
+    filter:{
+        text:" "
+    }
 }
 
 const FilterProvider = ({children})=>{
@@ -22,11 +27,23 @@ const FilterProvider = ({children})=>{
         dispatch({type:"SORTED_MOVIES"})
     }
 
+    const onChangeSearch = (event)=>{
+        let name = event.target.name
+        let value = event.target.value
+
+        return dispatch({type:"ON_SEARCH_CHANGE" , payload:{name,value}})
+    }
+
+    useEffect(()=>{
+        dispatch({type:"SEARCH_FILTER_MOVIES"})
+        dispatch({type:"SORTING_VALUE",payload:allmovies})
+    },[allmovies ,state.sorting_value,state.filter])
+
     useEffect(()=>{
         dispatch({type:"FILTER_MOVIES",payload:allmovies})
     },[allmovies])
 
-    return <FilterContext.Provider value={{...state , sorting}}>
+    return <FilterContext.Provider value={{...state , sorting , onChangeSearch}}>
         {children}
     </FilterContext.Provider>
 }
