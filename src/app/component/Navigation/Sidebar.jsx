b081119e9d4a6e8a37b9542d808dc54635742ad0
itsfarhankhan28 <famannat@gmail.com>
2023-08-20 21:06:44 +0530

    feat(Sidebar): changed content of sidebar

--- src/app/component/Navigation/Sidebar.jsx
+++ src/app/component/Navigation/Sidebar.jsx
@@ -12,21 +12,37 @@ const Sidebar = () => {
     let newval = data.map((curelem)=>{
       return curelem.director
     })
-    newval = [...new Set(newval)]
-    console.log(newval)
+    return newval = [...new Set(newval)]
+    // console.log(newval)
   }
   console.log(allmovies)
   const filterMovies = getuniquedata(allmovies)
+  console.log(filterMovies)
 
   return (
     <>
-    <div className='py-20 flex justify-center  lg:visible'>
-        <div className='flex flex-col gap-8 justify-center items-center h-[80%] fixed'>
-            <Avatar className='shadow-xl' style={{backgroundColor:'gray',width:'60px',height:'60px'}}>N</Avatar>
-            <Avatar className='shadow-xl' style={{backgroundColor:'gray',width:'60px',height:'60px'}}>N</Avatar>
-            <Avatar className='shadow-xl' style={{backgroundColor:'gray',width:'60px',height:'60px'}}>N</Avatar>
-            <Avatar className='shadow-xl' style={{backgroundColor:'gray',width:'60px',height:'60px'}}>N</Avatar>
-            <Avatar className='shadow-xl' style={{backgroundColor:'gray',width:'60px',height:'60px'}}>N</Avatar>
+    <div className='flex justify-center lg:visible px-5'>
+        <div className='flex flex-col gap-8 justify-center items-center h-screen fixed overflow-y-scroll'>
+          <h1 className='text-gray-400 text-center text-xl'>See what your favourite Directors got for you</h1>
+          {filterMovies?.map((item)=>{
+            console.log(item)
+            return(
+              <>
+                <button
+                // key={index}
+                type='button'
+                name='director'
+                value={item}
+                >
+                  <div className='flex flex-col items-center gap-1 cursor-pointer'>
+                    <Avatar className='shadow-xl' style={{backgroundColor:'gray',width:'60px',height:'60px'}}>N</Avatar>
+                    <h1 className='text-gray-400'>{item}</h1>
+                  </div>
+                </button>
+              </>
+            )
+          })}
+          {/* {filterMovies} */}
         </div>
     </div> 
     </>
