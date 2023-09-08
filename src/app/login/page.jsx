b35d12f349b7631a58526a084d7be64929f9bcbc
itsfarhankhan28 <famannat@gmail.com>
2023-09-08 08:23:09 +0530

    feat(login): created login form

---
+++ src/app/login/page.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+'use client'
+
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+import { NextResponse } from "next/server"
+import axios from 'axios'
+
+const page = () => {
+    const router = useRouter()
+    const [user,setUser] = useState({
+        email:"",
+        password:""
+    })
+
+    const onLogin = async()=>{
+        try{
+            const response = await axios.post('/api/users/login',user)
+            console.log(response)
+            NextResponse.json(response,"User Login successful")
+            router.push('/secondpage')
+        }catch(err){
+            return NextResponse.json({error:"Error in Logging In"})
+        }
+    }
+
+  return (
+    <div className="flex justify-center items-center h-screen border">
+      <div className="p-5 border border-black">
+        <div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input 
+                type="text" 
+                name="email"
+                value={user.email}
+                onChange={(e)=>setUser({...user,email:e.target.value})}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input 
+                type="password" 
+                name="password"
+                value={user.password}
+                onChange={(e)=>setUser({...user,password:e.target.value})}
+                />
+            </div>
+            <div>
+                <button onClick={onLogin}>
+                    Login
+                </button>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
