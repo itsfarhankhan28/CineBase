e6961180e5f81f696c52743e773a8171df6faf06
itsfarhankhan28 <famannat@gmail.com>
2023-09-08 08:21:10 +0530

    fix(signup): added await in the async fucntion

--- src/app/api/users/signup/route.js
+++ src/app/api/users/signup/route.js
@@ -7,7 +7,7 @@ connect()
 
 export async function POST(request){
     try{
-        const reqBody = request.json()
+        const reqBody = await request.json()
 
         const {username,email,password} = reqBody
 
