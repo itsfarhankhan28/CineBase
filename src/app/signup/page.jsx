517972f11933e17e559a0d52916b2c9eba62e996
itsfarhankhan28 <famannat@gmail.com>
2023-09-08 08:22:35 +0530

    feat(signup): created the signup form

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+'use client'
+
+import { useState } from "react"
+import axios from 'axios'
+import { useRouter } from "next/navigation"
+
+const page = () => {
+    const router = useRouter()
+    const [user , setUser] = useState({
+        username:"",
+        email:"",
+        password:""
+    })
+
+    const onSignup = async()=>{
+        try{
+            const response = await axios.post('/api/users/signup',user)
+            console.log("Signup successful",response.data)
+            router.push('/login')
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+  return (
+    <>
+    <div className="flex justify-center items-center h-screen">
+        <div className="p-5 border border-black">
+            <h1>Signup page</h1>
+            <form>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input 
+                    type="text" 
+                    name="username"
+                    value={user.username}
+                    onChange={(e)=>setUser({...user,username:e.target.value})}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input 
+                    type="text" 
+                    name="email"
+                    value={user.email}
+                    onChange={(e)=>setUser({...user,email:e.target.value})}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input 
+                    type="password" 
+                    name="password"
+                    value={user.password}
+                    onChange={(e)=>setUser({...user,password:e.target.value})}
+                    />
+                </div>
+                <div>
+                    <button onClick={onSignup}>
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>  
+    </>
+  )
+}
+
+export default page
