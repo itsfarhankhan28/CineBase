77fa4aaa1bf57372f55068d276ff94d3e67ef722
itsfarhankhan28 <famannat@gmail.com>
2023-09-10 11:30:43 +0530

    style(login): changed style of login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/rules-of-hooks */
 'use client'
 
@@ -26,11 +27,19 @@ const page = () => {
 
   return (
     <div className="flex justify-center items-center h-screen border">
-      <div className="p-5 border border-black">
+      <div className="p-5 flex flex-col gap-3">
         <div>
+            <img className="w-[200px]" src="/IMG/authimg (2).png" alt="" />
+        </div>
+        <div className="leading-10">
+            <h1 className="text-3xl font-semibold">Welcome back</h1>
+            <h1 className="text-md text-gray-400">Please enter your email and password</h1>
+        </div>
+        <div className="flex gap-5">
             <div>
-                <label htmlFor="email">Email</label>
                 <input 
+                className="w-full p-3 border-2 border-gray-400 rounded-md"
+                placeholder="Email"
                 type="text" 
                 name="email"
                 value={user.email}
@@ -38,19 +47,20 @@ const page = () => {
                 />
             </div>
             <div>
-                <label htmlFor="password">Password</label>
-                <input 
+                <input
+                className="w-full p-3 border-2 border-gray-400 rounded-md"
+                placeholder="Password" 
                 type="password" 
                 name="password"
                 value={user.password}
                 onChange={(e)=>setUser({...user,password:e.target.value})}
                 />
             </div>
-            <div>
-                <button onClick={onLogin}>
-                    Login
-                </button>
-            </div>
+        </div>
+        <div className="mt-3 flex justify-end">
+            <button className="border-2 border-gray-400 rounded-md px-7 py-3 font-semibold text-gray-400" onClick={onLogin}>
+                Login
+            </button>
         </div>
       </div>
     </div>
