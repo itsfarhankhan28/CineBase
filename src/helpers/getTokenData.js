e0f0a5cf8d97d4222e999996605e3a8b481a114c
itsfarhankhan28 <famannat@gmail.com>
2023-09-11 10:52:49 +0530

    feat(helpers): created helper to get the token data

---
+++ src/helpers/getTokenData.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import jwt from 'jsonwebtoken'
+
+export function getTokenData(request){
+    try{
+        const token = request.cookies.get("token")?.value || ""
+        const decodeddata = jwt.verify(token, process.env.SECRET_KEY)
+        return decodeddata.id
+    }catch(error){
+        return NextResponse.json({error:error.message})
+    }
+}
