13476e945b6cf4b6482129196e1e4b4c52c52fb8
itsfarhankhan28 <famannat@gmail.com>
2023-09-12 16:26:40 +0530

    fix(helpers): extract email instead of ID from token

--- src/helpers/getTokenData.js
+++ src/helpers/getTokenData.js
@@ -5,7 +5,7 @@ export function getTokenData(request){
     try{
         const token = request.cookies.get("token")?.value || ""
         const decodeddata = jwt.verify(token, process.env.SECRET_KEY)
-        return decodeddata.id
+        return decodeddata.email
     }catch(error){
         return NextResponse.json({error:error.message})
     }
