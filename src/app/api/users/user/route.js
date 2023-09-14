6a7c0cb3612ebbde9bcf4fb86ca6cce6f8a50e16
itsfarhankhan28 <famannat@gmail.com>
2023-09-14 15:46:16 +0530

    feat(user): added the functionality of helper in user

--- src/app/api/users/user/route.js
+++ src/app/api/users/user/route.js
@@ -1,18 +1,24 @@
 import { NextResponse } from "next/server";
 import connect from '@/dbConfig/dbConfig'
 import User from '@/models/userschema'
-import { getTokenData } from "@/helpers/getTokenData";
+import jwt from "jsonwebtoken";
 
 connect()
 
 export async function GET(request){
     try{
-        const email = await getTokenData(request)
-        console.log('extracted email',email)
+        //we are retrieving the token
+        const token = request.cookies.get('token')?.value || ''
+        //decoding the token
+        const decodeddata = jwt.verify(token,process.env.SECRET_KEY)
+        //storing email
+        const email = decodeddata.email
+        console.log(email)
+        //checking user in database in the bases of email
         const user = await User.findOne({email:email}).select("-password")
-        console.log(user)
+        //returning the user details
         return NextResponse.json({
-            message:true,
+            message:"User found",
             data:user
         })
     }catch(error){
