da968a6c9adf947e7aa5b2f8f7819ff253164274
itsfarhankhan28 <famannat@gmail.com>
2023-09-16 16:14:49 +0530

    refactor(user): extracting ID instead of email

--- src/app/api/users/user/route.js
+++ src/app/api/users/user/route.js
@@ -9,16 +9,17 @@ export async function GET(request){
     try{
         //we are retrieving the token
         const token = request.cookies.get('token').value || ''
+        console.log(token)
         //decoding the token
         const decodeddata = JSON.parse(
             Buffer.from(token.split(".")[1], "base64").toString()
           );
         console.log(decodeddata)
         //storing email
-        const email = decodeddata.email
-        console.log(email)
+        const userId = decodeddata.id
+        console.log(userId)
         //checking user in database in the bases of email
-        const user = await User.findOne({email:email}).select("-password")
+        const user = await User.findOne({_id:userId}).select("-password")
         //returning the user details
         return NextResponse.json({
             message:"User found",
