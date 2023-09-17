019288967e44b8d821e8608c363ec0b1826a008c
itsfarhankhan28 <famannat@gmail.com>
2023-09-17 13:40:55 +0530

    refactor(user): commented token retriving part

--- src/app/api/users/user/route.js
+++ src/app/api/users/user/route.js
@@ -1,22 +1,22 @@
 import { NextResponse } from "next/server";
 import connect from '@/dbConfig/dbConfig'
 import User from '@/models/userschema'
-import jwt from 'jsonwebtoken'
+// import jwt from 'jsonwebtoken'
+import { getTokenData } from "@/helpers/getTokenData";
 
 connect()
 
 export async function GET(request){
     try{
         //we are retrieving the token
-        const token = request.cookies.get('token').value || ''
-        console.log(token)
-        //decoding the token
-        const decodeddata = JSON.parse(
-            Buffer.from(token.split(".")[1], "base64").toString()
-          );
-        console.log(decodeddata)
+        // const token = request.cookies.get('token').value || ''
+        // console.log(token)
+        // //decoding the token
+        // const decodeddata = jwt.decode(token)
+        // console.log(decodeddata)
         //storing email
-        const userId = decodeddata.id
+
+        const userId = await getTokenData(request)
         console.log(userId)
         //checking user in database in the bases of email
         const user = await User.findOne({_id:userId}).select("-password")
