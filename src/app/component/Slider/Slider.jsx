c3bd43850d8fd21eca246ef53077d88495beae5f
itsfarhankhan28 <famannat@gmail.com>
2023-09-19 10:47:18 +0530

    style(Slider): changed style of slider

--- src/app/component/Slider/Slider.jsx
+++ src/app/component/Slider/Slider.jsx
@@ -4,7 +4,7 @@ import Carousel from 'nuka-carousel';
 const Slider = () => {
   return (
     <>
-    <div className="w-[1000px] h-[500px] mx-auto rounded-2xl flex justify-center items-center shadow-lg bg-[url('/IMG/textcarouselimg.jpg')] bg-cover">
+    <div className="w-[1100px] h-[500px] mx-auto rounded-2xl flex justify-center items-center shadow-lg bg-[url('/IMG/textcarouselimg.jpg')] bg-cover">
       <h1>Slider</h1>
     </div>
     </>
