e2ea693b0fd012335e56b5af67b4f44fbf18dfda
itsfarhankhan28 <famannat@gmail.com>
2023-09-19 23:37:23 +0530

    feat(Slider): created Slider

--- src/app/component/Slider/Slider.jsx
+++ src/app/component/Slider/Slider.jsx
@@ -1,14 +1,70 @@
-import React from 'react'
-import Carousel from 'nuka-carousel';
+/* eslint-disable @next/next/no-img-element */
 
-const Slider = () => {
+import { useQuery } from 'react-query';
+import axios from 'axios'
+import Loader from '../Loader/Loader';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css/core';
+
+const Slider = ()=> {
+
+  const {isLoading,isError,data} = useQuery(
+    'CarouselImage',
+    async()=>{
+      const movieposter = await axios.get('https://moviesapi3.onrender.com/movies?genres=Sci-Fi')
+      return movieposter.data
+    }
+  )
+
+  if(isLoading){
+    return (
+      <Loader/>
+    )
+  }
+
+  if(isError){
+    return (
+      <h1>Error...</h1>
+    )
+  }
+  console.log(data.map((item)=>{
+    return(
+      item.media.map((items)=>{
+        return(
+          items.imageurl
+        )
+      })
+    )
+  }))
+
   return (
     <>
-    <div className="w-[1100px] h-[500px] mx-auto rounded-2xl flex justify-center items-center shadow-lg bg-[url('/IMG/textcarouselimg.jpg')] bg-cover">
-      <h1>Slider</h1>
-    </div>
+
+      <div className='w-[1100px] h-[500px] mx-auto'>
+        <Splide
+        options={{
+          autoplay:true,
+          type:'loop',
+          pauseOnHover:true,
+          arrows:false
+        }}>
+          {data.map((items)=>{
+            return (
+              items.media.map((item)=>{
+                return (
+                  <>
+                    <SplideSlide data-splide-interval="2000">
+                      <img className='h-[500px] w-[1100px] rounded-2xl' src={`${item.imageurl}`} alt="Image 1"/>
+                    </SplideSlide>
+                  </>
+                )
+              })
+            )
+          })}
+        </Splide>
+      </div>
     </>
-  )
+  );
 }
 
-export default Slider
+export default Slider;
