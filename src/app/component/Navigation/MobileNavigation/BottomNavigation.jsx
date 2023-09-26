e3b93e47554560ecb3a00fda5cf97168997594b8
itsfarhankhan28 <famannat@gmail.com>
2023-09-26 17:54:17 +0530

    style(BottomNavigation): changed style

--- src/app/component/Navigation/MobileNavigation/BottomNavigation.jsx
+++ src/app/component/Navigation/MobileNavigation/BottomNavigation.jsx
@@ -8,24 +8,24 @@ import {FiLogOut} from 'react-icons/fi'
 
 const BottomNavigation = () => {
   return (
-    <div className='w-full h-[150px] bg-[#F7F8FF] border border-black bottom-0 fixed p-3 flex justify-center items-center'>
-        <div className='flex gap-3 justify-center items-center'>
-            <div className='text-2xl'>
+    <div className='w-full h-[100px] bg-[#F7F8FF] bottom-0 fixed flex justify-center items-center z-50 shadow-xl border-t-2 border-gray-200'>
+        <div className='flex gap-5 justify-center items-center'>
+            <div className='text-[40px]'>
                 <HiOutlineSearchCircle/>
             </div>
-            <div className='text-2xl'>
+            <div className='text-[40px]'>
                 <MdUpcoming/>
             </div>
-            <div className='text-2xl'>
+            <div className='text-[40px]'>
                 <GiThreeFriends/>
             </div>
-            <div className='text-2xl'>
+            <div className='text-[40px]'>
                 <HiUserGroup/>
             </div>
-            <div className='text-2xl'>
+            <div className='text-[40px]'>
                 <IoIosSettings/>
             </div>
-            <div className='text-2xl'>
+            <div className='text-[40px]'>
                 <FiLogOut/>
             </div>
         </div>
