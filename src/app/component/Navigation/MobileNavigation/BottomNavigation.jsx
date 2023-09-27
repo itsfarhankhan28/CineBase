b84f0c53d08ee249c44077dce48ddba7fbc4dcb6
itsfarhankhan28 <famannat@gmail.com>
2023-09-27 15:57:42 +0530

    style(BottomNavigation): changed style of bottom navbar for reponsive design

--- src/app/component/Navigation/MobileNavigation/BottomNavigation.jsx
+++ src/app/component/Navigation/MobileNavigation/BottomNavigation.jsx
@@ -8,24 +8,24 @@ import {FiLogOut} from 'react-icons/fi'
 
 const BottomNavigation = () => {
   return (
-    <div className='w-full h-[100px] bg-[#F7F8FF] bottom-0 fixed flex justify-center items-center z-50 shadow-xl border-t-2 border-gray-200'>
-        <div className='flex gap-5 justify-center items-center'>
-            <div className='text-[40px]'>
+    <div className='w-full h-[70px] bg-[#F7F8FF] bottom-0 fixed flex justify-center items-center z-50 shadow-xl border-t-2 border-gray-200'>
+        <div className='flex gap-7 justify-center items-center'>
+            <div className='text-[35px] text-gray-400'>
                 <HiOutlineSearchCircle/>
             </div>
-            <div className='text-[40px]'>
+            <div className='text-[35px] text-gray-400'>
                 <MdUpcoming/>
             </div>
-            <div className='text-[40px]'>
+            <div className='text-[35px] text-gray-400'>
                 <GiThreeFriends/>
             </div>
-            <div className='text-[40px]'>
+            <div className='text-[35px] text-gray-400'>
                 <HiUserGroup/>
             </div>
-            <div className='text-[40px]'>
+            <div className='text-[35px] text-gray-400'>
                 <IoIosSettings/>
             </div>
-            <div className='text-[40px]'>
+            <div className='text-[35px] text-gray-400'>
                 <FiLogOut/>
             </div>
         </div>
