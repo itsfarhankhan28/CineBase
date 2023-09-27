cd5826b744dba05de0f657eea70bc199d6d58efc
itsfarhankhan28 <famannat@gmail.com>
2023-09-27 15:58:40 +0530

    style(Display): changed style

--- src/app/component/Cards/CardsDisplay/Display.jsx
+++ src/app/component/Cards/CardsDisplay/Display.jsx
@@ -4,11 +4,11 @@ const Display = (props) => {
   return (
     <div className='min-h-[110px] w-full p-3 backdrop-blur-3xl text-white rounded-br-lg rounded-bl-lg flex flex-col justify-between'>
       <div>
-        <h1 className='font-semibold text-lg'>{props.moviename}</h1>
+        <h1 className='font-semibold lg:text-lg'>{props.moviename}</h1>
       </div>
       <div className='flex justify-between'>
-        <h1 className='text-lg'>{props.year}</h1>
-        <h1 className='text-lg'>{props.ratings}</h1>
+        <h1 className='lg:text-lg'>{props.year}</h1>
+        <h1 className='lg:text-lg'>{props.ratings}</h1>
       </div>
     </div>
   )
