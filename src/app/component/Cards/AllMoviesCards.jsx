f5330b00b3b1a17c792a11360cf99c67ac7dcd25
itsfarhankhan28 <famannat@gmail.com>
2023-09-28 16:33:34 +0530

    feat(AllMoviesCards): added display component

--- src/app/component/Cards/AllMoviesCards.jsx
+++ src/app/component/Cards/AllMoviesCards.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Display from './CardsDisplay/Display'
 
 const AllMoviesCards = (props) => {
   return (
     <div>
         <div
         style={{'--image-url': `url(${props.movieposter})`}}  
-        className={`w-[300px] h-[300px] rounded-2xl shadow-xl flex justify-center items-center bg-[image:var(--image-url)] bg-center bg-cover cursor-pointer`}>
-            {/* <h1>{props.moviename}</h1> */}
+        className={`lg:w-[350px] xxsm:w-[165px] xxsm:h-[300px] lg:h-[350px] rounded-2xl shadow-xl flex justify-center items-end bg-[image:var(--image-url)] bg-center bg-cover cursor-pointer`}>
+            <Display moviename={props.moviename} year={props.year} ratings={props.ratings}/>
         </div>
     </div>
   )
