015d2a1aea6835b3fea886300986a13a394dbe9a
itsfarhankhan28 <famannat@gmail.com>
2023-09-28 16:30:29 +0530

    feat(page): added bottom and top navigation for reponsive design

--- src/app/secondpage/page.jsx
+++ src/app/secondpage/page.jsx
@@ -8,27 +8,41 @@ import Sidebar from '../component/Navigation/Sidebar'
 import TopNavbar from '../component/Navigation/TopNavbar'
 // import { AllMoviesContext, BrowseMoviesContext } from '../context/AllMoviesContext'
 import { FilterProvider } from '../context/FilterContext'
+import BottomNavigation from '../component/Navigation/MobileNavigation/BottomNavigation'
+import TopNavigation from '../component/Navigation/MobileNavigation/TopNavigation'
 
 const page = () => {
 
   return (
       <AppProvidertwo>
         <FilterProvider>
-          <div className='flex flex-row justify-between w-[100vw]'>
-            <div className='w-[15%]'>
+          <div className='flex flex-row justify-between lg:w-[100vw]'>
+            {/* Navigation for large screen */}
+            <div className='w-[15%] lg:visible lg:block hidden'>
               <Navbar/>
             </div>
-            <div className='w-[75%] h-auto min-h-screen bg-[#F7F8FF] overflow-y-visible pb-10'>
-              <div>
+
+            <div className='lg:hidden z-50'>
+              <BottomNavigation/>
+            </div>
+
+            {/* Top navigation for Mobile screen */}
+            <div className='lg:hidden z-50'>
+              <TopNavigation/>
+            </div>
+
+            <div className='lg:w-[85%] h-auto min-h-screen bg-[#F7F8FF] overflow-y-visible lg:pb-10 xxsm:pb-20 lg:pl-0 xxsm:pl-3'>
+              {/* Top navigation for large screen */}
+              <div className='lg:visible hidden lg:block'>
                 <TopNavbar/>
               </div>
-              <div className='mt-10'>
+              <div className='lg:mt-10 xxsm:mt-14'>
                 <AllMovies/>
               </div>
             </div>
-            <div className='w-[10%]'>
+            {/* <div className='w-[10%]'>
               <Sidebar/>
-            </div>
+            </div> */}
           </div>
         </FilterProvider>
       </AppProvidertwo>
