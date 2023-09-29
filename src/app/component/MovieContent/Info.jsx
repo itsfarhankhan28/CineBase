f71c2cda63ba31d7cbc142b51560b2a446a688a6
itsfarhankhan28 <famannat@gmail.com>
2023-09-29 16:22:34 +0530

    style(Info): changed style

--- src/app/component/MovieContent/Info.jsx
+++ src/app/component/MovieContent/Info.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Info = (props) => {
   return (
-    <div className='w-[1000px] h-[300px] flex gap-5 p-10'>
-      <div className='w-[30%]'>
-        <img className='w-full h-auto rounded-2xl' src={`${props.poster}`} alt="" />
+    <div className='lg:w-[1000px] lg:h-[300px] flex lg:flex-row xxsm:flex-col gap-5 p-10'>
+      <div className='lg:w-[30%]'>
+        <img className='lg:w-full lg:h-auto xxsm:h-[250px] xxsm:w-[250px] rounded-2xl xxsm:shadow-xl' src={`${props.poster}`} alt="" />
       </div>
-      <div className='w-[70%] flex flex-col gap-3'>
+      <div className='lg:w-[70%] flex flex-col gap-3'>
         <div>
           <h1 className='font-semibold text-3xl'>{props.moviename}</h1>
         </div>
@@ -15,11 +15,11 @@ const Info = (props) => {
           <p>{props.shortdescription}</p>
         </div>
           <div>
-            <div className='flex justify-between w-[400px]'>
+            <div className='flex xxsm:flex-col lg:flex-row xxsm:gap-1 lg:gap-0 justify-between lg:w-[400px]'>
               <h1 className='font-semibold'>Ratings:<span className='font-normal'>{props.ratings}</span></h1>
-              <div>|</div>
+              <div className='lg:visible lg:block hidden'>|</div>
               <h1 className='font-semibold'>Runtime:<span className='font-normal'>{props.movietime}</span></h1>
-              <div>|</div>
+              <div className='lg:visible lg:block hidden'>|</div>
               <h1 className='font-semibold'>Released In:<span className='font-normal'>{props.year}</span></h1>
             </div>
           </div>
