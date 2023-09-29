ab09f1832d6a667daf816c99b5fa2b07e9491664
itsfarhankhan28 <famannat@gmail.com>
2023-09-29 16:18:25 +0530

    style(page): changed style

--- src/app/singlemovie/[id]/page.jsx
+++ src/app/singlemovie/[id]/page.jsx
@@ -9,6 +9,8 @@ import axios from 'axios'
 import { useEffect,useState } from 'react'
 import Description from '@/app/component/MovieContent/Description'
 import Actors from '@/app/component/MovieContent/Actors'
+import BottomNavigation from '@/app/component/Navigation/MobileNavigation/BottomNavigation'
+import TopNavigation from '@/app/component/Navigation/MobileNavigation/TopNavigation'
 
 const page = ({params}) => {
     const [singledata,setSingledata] = useState()
@@ -35,12 +37,18 @@ const page = ({params}) => {
   return (
     <>
         <div className='flex justify-between w-[100vw]'>
-            <div className='w-[15%]'>
+            <div className='lg:hidden'>
+                <TopNavigation/>
+            </div>
+            <div className='w-[15%] lg:visible lg:block hidden'>
                 <Navbar/>
             </div>
-            <div className='w-[85%] h-auto bg-[#F7F8FF] overflow-y-visible pb-10 flex flex-col gap-10'>
+            <div className='lg:hidden z-50'>
+                <BottomNavigation/>
+            </div>
+            <div className='lg:w-[85%] h-auto bg-[#F7F8FF] overflow-y-visible pb-16 flex flex-col lg:gap-10 xxsm:pt-7 lg:pt-0'>
                 {/* Movie trailer */}
-                <div>
+                <div className='lg:visible lg:block hidden'>
                     {singledata.media.map((items)=>{
                         return (
                             <Trailer video={items.videourl}/>
@@ -64,11 +72,11 @@ const page = ({params}) => {
                     })}
                 </div>
                 {/* Movies Description and Actors,Directors and Producers */}
-                <div className='flex gap-5 mx-auto w-[1000px] p-5'>
-                    <div className=' w-[50%]'>
+                <div className='flex lg:flex-row xxsm:flex-col-reverse gap-5 mx-auto lg:w-[1000px] p-5 z-10'>
+                    <div className=' lg:w-[50%] lg:py-0 xxsm:px-3'>
                         <Description storyline={singledata.storyline}/>
                     </div>
-                    <div className=' w-[50%]'>
+                    <div className=' lg:w-[50%]'>
                         <Actors 
                         actors={singledata.stars}
                         director={singledata.director}
