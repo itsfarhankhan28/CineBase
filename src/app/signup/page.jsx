93b6bf60f22adc3e9d7c39c4067aac5ae11ff2f4
itsfarhankhan28 <famannat@gmail.com>
2023-09-30 20:42:32 +0530

    style(page): changed style

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -7,6 +7,8 @@ import axios from 'axios'
 import { useRouter } from "next/navigation"
 import { NextResponse } from "next/server"
 import Navbar from "../component/Navigation/Navbar"
+import BottomNavigation from "../component/Navigation/MobileNavigation/BottomNavigation"
+import TopNavigation from "../component/Navigation/MobileNavigation/TopNavigation"
 
 const page = () => {
     const router = useRouter()
@@ -31,16 +33,22 @@ const page = () => {
     <>
 
     <div className="flex flex-row justify-between w-[100vw]">
-        <div className="w-[15%]">
+        <div className="lg:hidden">
+            <TopNavigation/>
+        </div>
+        <div className="lg:w-[15%] lg:visible lg:block hidden">
             <Navbar/>
         </div>
-        <div className="w-[85%] bg-[#F7F8FF]">
-        <div className="flex justify-center items-center h-screen">
+        <div className="lg:hidden">
+            <BottomNavigation/>
+        </div>
+        <div className="lg:w-[85%] bg-[#F7F8FF]">
+        <div className="flex justify-center items-center h-screen xxsm:pb-5 lg:pb-0">
         <div className="p-5 h-auto w-auto flex flex-col gap-5">
-            <div><img className="w-[200px]" src='/IMG/authimg (2).png' alt="" /></div>
+            <div><img className="lg:w-[200px] xxsm:w-[100px]" src='/IMG/authimg (2).png' alt="" /></div>
             <div className="leading-10">
-                <h1 className="text-3xl font-semibold">Welcome, Create your account</h1>
-                <h4 className="text-md text-gray-400">Enter your email and create a password</h4>
+                <h1 className="lg:text-3xl xxsm:text-2xl font-semibold">Welcome, Create your account</h1>
+                <h4 className="lg:text-md text-gray-400">Enter your email and create a password</h4>
             </div>
             <div className="flex flex-col gap-5">
                 <div>
@@ -53,7 +61,7 @@ const page = () => {
                     onChange={(e)=>setUser({...user,username:e.target.value})}
                     />
                 </div>
-                <div className="flex gap-5">
+                <div className="flex gap-5 lg:flex-row xxsm:flex-col">
                     <div>
                         <input 
                         className="w-full border-2 border-gray-400 p-3 rounded-lg"
