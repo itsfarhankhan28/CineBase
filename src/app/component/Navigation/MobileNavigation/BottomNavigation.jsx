910c26d4ae5acf9cb36d0c0b5c7bdad98d0a0207
itsfarhankhan28 <famannat@gmail.com>
2023-09-30 20:41:49 +0530

    feat(BottomNavigation): added link to secondpage

--- src/app/component/Navigation/MobileNavigation/BottomNavigation.jsx
+++ src/app/component/Navigation/MobileNavigation/BottomNavigation.jsx
@@ -5,14 +5,17 @@ import {GiThreeFriends} from 'react-icons/gi'
 import {HiUserGroup} from 'react-icons/hi'
 import {IoIosSettings} from 'react-icons/io'
 import {FiLogOut} from 'react-icons/fi'
+import Link from 'next/link'
 
 const BottomNavigation = () => {
   return (
     <div className='w-full h-[70px] bg-[#F7F8FF] bottom-0 fixed flex justify-center items-center z-50 shadow-xl border-t-2 border-gray-200'>
         <div className='flex gap-7 justify-center items-center'>
-            <div className='text-[35px] text-gray-400'>
-                <HiOutlineSearchCircle/>
-            </div>
+            <Link href='/secondpage'>
+                <div className='text-[35px] text-gray-400'>
+                    <HiOutlineSearchCircle/>
+                </div>
+            </Link>
             <div className='text-[35px] text-gray-400'>
                 <MdUpcoming/>
             </div>
