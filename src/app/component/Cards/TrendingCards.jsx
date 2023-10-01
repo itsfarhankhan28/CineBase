e5d0eca4e23acc94b66e89c0d6555a4d46a18020
itsfarhankhan28 <famannat@gmail.com>
2023-10-01 16:04:18 +0530

    style(TrendingCards): changed style

--- src/app/component/Cards/TrendingCards.jsx
+++ src/app/component/Cards/TrendingCards.jsx
@@ -7,9 +7,9 @@ const TrendingCards = (props) => {
     <div className='flex gap-2 lg:w-[500px]'>
         <div
         style={{'--image-url': `url(${props.movieposter})`}}  
-        className={`lg:w-[50%] xxsm:w-[165px] lg:h-[250px] xxsm:h-[300px] rounded-2xl shadow-xl flex items-end bg-[image:var(--image-url)] bg-center bg-cover cursor-pointer`}>
+        className={`lg:w-[50%] xxsm:w-[165px] lg:h-[250px] xxsm:h-[250px] rounded-2xl shadow-xl flex items-end bg-[image:var(--image-url)] bg-center bg-cover cursor-pointer`}>
         </div>
-        <div className='lg:w-[50%]'>
+        <div className='lg:w-[50%] xxsm:w-min'>
         <Display moviename={props.moviename} year={props.year} ratings={props.ratings}/>
         </div>
     </div>  
