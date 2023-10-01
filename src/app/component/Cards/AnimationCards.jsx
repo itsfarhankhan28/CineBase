b84329f4e60a5858d0191f7cb93b3bb9da697278
itsfarhankhan28 <famannat@gmail.com>
2023-10-01 11:48:52 +0530

    style(AnimationCards): changed style

--- src/app/component/Cards/AnimationCards.jsx
+++ src/app/component/Cards/AnimationCards.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Display from './CardsDisplay/Display'
+import DisplayTwo from './CardsDisplay/DisplayTwo'
 
 const AnimationCards = (props) => {
   return (
     <>
-    <div>
+    <div className='flex flex-col gap-2 h-[250px]'>
         <div
         style={{'--image-url': `url(${props.movieposter})`}}  
-        className={`lg:w-[350px] xxsm:w-[165px] lg:h-[350px] xxsm:h-[300px] rounded-2xl shadow-xl flex items-end bg-[image:var(--image-url)] bg-center bg-cover cursor-pointer`}>
-            <Display moviename={props.moviename} ratings={props.ratings} year={props.year}/>
+        className={`lg:w-[350px] xxsm:w-[165px] lg:h-[90%] xxsm:h-[300px] rounded-2xl shadow-xl flex items-end bg-[image:var(--image-url)] bg-center bg-cover cursor-pointer`}>
+        </div>
+        <div className='h-[10%]'>
+          <DisplayTwo moviename={props.moviename}/>
         </div>
     </div>  
     </>
