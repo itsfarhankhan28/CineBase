3d93ad3dee4f6ad313a9a09afea3fb6050cc07d7
itsfarhankhan28 <famannat@gmail.com>
2023-10-01 14:37:33 +0530

    style(SciFiCards): changed style

--- src/app/component/Cards/SciFiCards.jsx
+++ src/app/component/Cards/SciFiCards.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Display from './CardsDisplay/Display'
+import DisplayTwo from './CardsDisplay/DisplayTwo'
 
 const SciFiCards = (props) => {
   return (
     <>
-    <div>
+    <div className='flex flex-col gap-2 h-[250px]'>
         <div
         style={{'--image-url': `url(${props.movieposter})`}}  
-        className={`lg:w-[350px] xxsm:w-[165px] xxsm:h-[300px] lg:h-[350px] rounded-2xl shadow-xl flex items-end bg-[image:var(--image-url)] bg-center bg-cover cursor-pointer`}>
-            <Display moviename={props.moviename} ratings={props.ratings} year={props.year}/>
+        className={`lg:w-[350px] xxsm:w-[165px] xxsm:h-[300px] lg:h-[90%] rounded-2xl shadow-xl flex items-end bg-[image:var(--image-url)] bg-center bg-cover cursor-pointer`}>
+            {/* <Display moviename={props.moviename} ratings={props.ratings} year={props.year}/> */}
+        </div>
+        <div className='h-[10%]'>
+          <DisplayTwo moviename={props.moviename}/>
         </div>
     </div>  
     </>
