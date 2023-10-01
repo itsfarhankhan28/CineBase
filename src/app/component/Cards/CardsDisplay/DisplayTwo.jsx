993811a642b9a4ce74ad1c22c6e2840aabb6f22d
itsfarhankhan28 <famannat@gmail.com>
2023-10-01 11:50:26 +0530

    feat(DisplayTwo): created new display for cards

---
+++ src/app/component/Cards/CardsDisplay/DisplayTwo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DisplayTwo = (props) => {
+  return (
+    <div>
+        <h1 className='font-semibold '>{props.moviename}</h1>
+    </div>
+  )
+}
+
+export default DisplayTwo
