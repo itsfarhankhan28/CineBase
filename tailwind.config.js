5679cc4f9f17afe09a53803786e6d95a7fff1edd
itsfarhankhan28 <famannat@gmail.com>
2023-10-02 14:01:53 +0530

    feat(tailwind): added new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily:{
+        'Carousel-font':['Montserrat']
+      }
     },
   },
   plugins: [],
