93003edca82509a6c4a2c4200a4d16813a83ae70
itsfarhankhan28 <famannat@gmail.com>
2023-10-06 00:36:30 +0530

    feat(page): created page for upcoming movies

---
+++ src/app/comingsoonmovies/page.jsx
@@ -0,0 +1,25 @@
+'use client'
+
+import React from 'react'
+import Navbar from '../component/Navigation/Navbar'
+import BottomNavigation from '../component/Navigation/MobileNavigation/BottomNavigation'
+import ComingSoon from '../sections/ComingSoon'
+
+
+const page = () => {
+  return (
+    <div className='flex'>
+        <div className='w-[15%] lg:visible lg:block hidden'>
+            <Navbar/>
+        </div>
+        <div className='lg:hidden z-50'>
+            <BottomNavigation/>
+        </div>
+        <div className='lg:w-[85%] h-auto min-h-screen bg-[#F7F8FF] overflow-y-visible lg:pb-10 xxsm:pb-20 lg:pl-0 xxsm:pl-3'>
+            <ComingSoon/>
+        </div>
+    </div>
+  )
+}
+
+export default page
