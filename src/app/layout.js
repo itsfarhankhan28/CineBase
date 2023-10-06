fd1df464abe832d83a14d304a1632395e9ccfb22
itsfarhankhan28 <famannat@gmail.com>
2023-10-06 16:20:18 +0530

    fix(layout): corrected the import path of queryprovider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Queryclientprovider from './QueryClientProvider'
+import Queryclientprovider from './Queryclientprovider'
 
 const inter = Inter({ subsets: ['latin'] })
 
