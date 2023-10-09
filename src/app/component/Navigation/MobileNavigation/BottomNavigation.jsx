55708599b35e6434736fb92adff5f0717bfc1da4
itsfarhankhan28 <famannat@gmail.com>
2023-10-09 19:07:24 +0530

    feat(BottomNavigation): added the condition for login and logout

--- src/app/component/Navigation/MobileNavigation/BottomNavigation.jsx
+++ src/app/component/Navigation/MobileNavigation/BottomNavigation.jsx
@@ -6,12 +6,28 @@ import {HiUserGroup} from 'react-icons/hi'
 import {IoIosSettings} from 'react-icons/io'
 import {FiLogOut} from 'react-icons/fi'
 import Link from 'next/link'
+import axios from 'axios'
+import { useState,useEffect } from 'react'
+import { FiLogIn } from 'react-icons/fi'
 
 const BottomNavigation = () => {
+
+    const [user,setUser] = useState()
+
+    const getUserdetails = async()=>{
+        const response = await axios.get('/api/users/user')
+        console.log(response.data.data.username)
+        setUser(response.data.data.username)
+    }
+
+    useEffect(()=>{
+        getUserdetails()
+    })
+
   return (
     <div className='w-full h-[70px] bg-[#F7F8FF] bottom-0 fixed flex justify-center items-center z-50 shadow-xl border-t-2 border-gray-200'>
         <div className='flex gap-7 justify-center items-center'>
-            <Link href='/secondpage'>
+            <Link href='/browsemovies'>
                 <div className='text-[35px] text-gray-400'>
                     <HiOutlineSearchCircle/>
                 </div>
@@ -29,7 +45,7 @@ const BottomNavigation = () => {
                 <IoIosSettings/>
             </div>
             <div className='text-[35px] text-gray-400'>
-                <FiLogOut/>
+                if(user ? <FiLogOut/> : <FiLogIn/>)
             </div>
         </div>
     </div>
