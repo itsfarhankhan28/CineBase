cc43b39ec82c218190bc21a65b66662b1881de26
itsfarhankhan28 <famannat@gmail.com>
2023-12-12 19:55:33 +0530

    feat(Queryclientprovider): imported Provider and store to setup redux store

--- src/app/Queryclientprovider.js
+++ src/app/Queryclientprovider.js
@@ -1,13 +1,17 @@
 'use client'
 
 import { QueryClientProvider,QueryClient } from "@tanstack/react-query";
+import { Provider } from 'react-redux'
+import store from '@/utils/store/store'
 
 const queryclient = new QueryClient()
 
 export default function Queryclientprovider({children}){
     return (
-        <QueryClientProvider client={queryclient}>
-            {children}
-        </QueryClientProvider>
+        <Provider store={store}>
+            <QueryClientProvider client={queryclient}>
+                {children}
+            </QueryClientProvider>
+        </Provider>
     )
 }
