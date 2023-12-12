14c7c62b29d72b9beecd2371a7e8d88fbd3e0ca1
itsfarhankhan28 <famannat@gmail.com>
2023-12-12 19:57:50 +0530

    feat(AllMoviesSlice): created Slice

---
+++ src/utils/slices/AllMoviesSlice.js
@@ -0,0 +1,40 @@
+import {createSlice,createAsyncThunk} from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const fetchmovies = createAsyncThunk('fetchedMovies',async()=>{
+    const response = await axios.get('https://movies-api-others.vercel.app/movies/get')
+    const data = response.data
+    return data
+})
+
+const AllMoviesSlice = createSlice({
+    name:'allmovies',
+    initialState:{
+        allmovies:[],
+        filteredmovies:[]
+    },
+    reducers:{
+        searchMovies:(state,action)=>{
+            state.filteredmovies = state.allmovies.filter((movies)=>{
+                return movies.moviename.toLowerCase().includes(action.payload.toLowerCase())
+            })
+        }
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(fetchmovies.pending,(state)=>{
+            state.status = 'pending'
+        }),
+        builder.addCase(fetchmovies.fulfilled,(state,action)=>{
+            state.status = 'successded',
+            state.allmovies = action.payload,
+            state.filteredmovies = action.payload
+        }),
+        builder.addCase(fetchmovies.rejected,(state)=>{
+            state.status = 'rejected'
+        })
+    }
+})
+
+export const {searchMovies} = AllMoviesSlice.actions
+
+export default AllMoviesSlice.reducer
