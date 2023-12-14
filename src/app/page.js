831b6b30f0cef814a3d7725a8e164907c6c13fea
itsfarhankhan28 <famannat@gmail.com>
2023-12-14 22:42:36 +0530

    refactor(page): uncommented Treding page

--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ export default function Home() {
             </div>
 
             <div className='mt-10'>
-              {/* <Trending/> */}
+              <Trending/>
             </div>
 
             <div className='mt-10'>
