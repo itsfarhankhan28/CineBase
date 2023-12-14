13d8a4ae766bf9262121f28d0547ff34d6c55d6a
itsfarhankhan28 <famannat@gmail.com>
2023-12-14 16:40:03 +0530

    feat(next.config): added theme according to the custom light and dark mode

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    theme: {
+      initialColorMode: 'light',
+      light: {
+        themeColor: '#F7F8FF',
+      },
+      dark: {
+        themeColor: '#575556',
+      },
+    },
+  };
