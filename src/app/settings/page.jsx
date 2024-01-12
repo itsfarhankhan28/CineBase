2812d23085fca483aab097e825e7b170870776af
itsfarhankhan28 <famannat@gmail.com>
2024-01-12 09:21:22 +0530

    feat(settings): removed dark mode feature from settings

--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -3,17 +3,8 @@
 
 import React from 'react'
 import Navbar from '../component/Navigation/Navbar'
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { useTheme } from 'next-themes';
 
 const page = () => {
-
-    // const {resolvedTheme,setTheme,theme} = useTheme()
-    const {theme, setTheme } = useTheme()
 
   return (
     <div className='flex h-screen w-full'>
@@ -23,36 +14,7 @@ const page = () => {
       <div className='w-[85%]'>
         <div className='px-10 py-5 flex flex-col gap-5'>
             <h1 className='text-5xl font-semibold text-gray-400'>Settings:-</h1>
-            <div>
-                <Accordion>
-                    <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                    >
-                    <Typography>Appearance</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails style={{display:"flex",justifyContent:"space-between",alignItems:"end"}}>
-                    <Typography className='font-semibold text-gray-400'>
-                        Enable Dark Mode
-                    </Typography>
-                    {/* <button onClick={()=>setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}>
-                        {resolvedTheme === 'dark' ? 'light' : 'dark'}
-                    </button> */}
-                      <section>
-                      {/* <button onClick={()=>setTheme("light")}>
-                        Light
-                      </button>
-                      <button onClick={()=>setTheme("dark")}>
-                        Dark
-                      </button> */}
-                      <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-      Toggle Theme
-    </button>
-                      </section>
-                    </AccordionDetails>
-                </Accordion>
-            </div>
+            <h1 className='text-3xl text-gray-300'>Coming Soon...</h1>
         </div>
       </div>
     </div>
