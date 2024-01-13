38c6ffafbc7c8a4b4490a4fe65b759bb6d7a3ce2
itsfarhankhan28 <famannat@gmail.com>
2024-01-14 00:17:26 +0530

    refactor(Navbar): refactored navbar code

--- src/app/component/Navigation/Navbar.jsx
+++ src/app/component/Navigation/Navbar.jsx
@@ -13,6 +13,41 @@ import Link from 'next/link'
 import { NextResponse } from 'next/server'
 import { useRouter } from 'next/navigation'
 import {FiLogIn} from 'react-icons/fi'
+import {BsFillPersonFill} from 'react-icons/bs'
+
+const navlinksMenu = [
+    {
+        name:'Browse',
+        href:'/browsemovies',
+        icon:<HiOutlineSearchCircle/>
+    },
+    {
+        name:'Coming Soon',
+        href:'/comingsoonmovies',
+        icon:<MdUpcoming/>
+    }
+]
+
+const navlinksSocial = [
+    {
+        name:'Friends',
+        href:'/friends',
+        icon:<GiThreeFriends/>
+    },
+    {
+        name:'Social',
+        href:'/social',
+        icon:<HiUserGroup/>
+    }
+]
+
+const navlinksGeneral = [
+    {
+        name:'Settings',
+        href:'/settings',
+        icon:<IoIosSettings/>
+    }
+]
  
 const Navbar = () => {
     const router = useRouter()
@@ -46,51 +81,75 @@ const Navbar = () => {
     },[])
 
   return (
-    <div className='border-r-2 border-gray-200 h-screen fixed py-10 w-[15%]'>
-      <div className='flex gap-5 flex-col h-[80%] items-center pl-5'>
+    <div className='border-r-2 border-gray-200 h-screen fixed py-10 w-[15%] flex flex-col justify-between'>
+      <div className='flex flex-col gap-5 h-[80%] items-center pl-5'>
         <div className='w-full h-[20%] flex gap-1 justify-start items-baseline'>
             <div className='text-[50px]'><BsCameraReelsFill/></div>
             <h1 className='text-[30px] font-bold'>CineBase</h1>
         </div>
         <div className='flex flex-col gap-3 w-full h-[30%]'>
             <h1 className='font-semibold text-xl text-gray-400'>Menu</h1>
-            <div className='flex gap-4 items-center'>
-                <div className='text-2xl text-gray-400'><HiOutlineSearchCircle/></div>
-                <Link href='/browsemovies'><div className='text-gray-400 font-semibold cursor-pointer'>Browse</div></Link>
-            </div>
-            <div className='flex gap-4 items-center'>
-                <div className='text-2xl text-gray-400'><MdUpcoming/></div>
-                <Link href='/comingsoonmovies'><div className='text-gray-400 font-semibold cursor-pointer'>Coming Soon</div></Link>
+            <div className='flex flex-col'>
+                {navlinksMenu.map((links)=>{
+                    return (
+                        <>
+                        <div className='flex gap-4 items-center p-2 hover:bg-slate-200 w-[170px] rounded-lg'>
+                            <div className='text-2xl text-gray-400'>{links.icon}</div> 
+                            <Link href={`${links.href}`}><div className='text-gray-400 font-semibold cursor-pointer text-lg'>{links.name}</div></Link>
+                        </div>
+                        </>
+                    )
+                })}
             </div>
         </div>
         <div className='flex flex-col gap-3 w-full h-[30%]'>
-            <h1 className='font-semibold text-xl text-gray-400 cursor-pointer'>Social</h1>
-            <div className='flex gap-4 items-center'>
-                <div className='text-2xl text-gray-400'><GiThreeFriends/></div>
-                <div className='text-gray-400 font-semibold cursor-pointer'>Friends</div>
-            </div>
-            <div className='flex gap-4 items-center'>
-                <div className='text-2xl text-gray-400'><HiUserGroup/></div>
-                <div className='text-gray-400 font-semibold cursor-pointer'>Social</div>
+            <h1 className='font-semibold text-xl text-gray-400'>Social</h1>
+            <div className='flex flex-col'>
+                {navlinksSocial.map((links)=>{
+                    return (
+                        <>
+                        <div className='flex gap-4 items-center p-2 hover:bg-slate-200 w-[170px] rounded-lg'>
+                            <div className='text-2xl text-gray-400'>{links.icon}</div> 
+                            <Link href={`${links.href}`}><div className='text-gray-400 font-semibold cursor-pointer text-lg'>{links.name}</div></Link>
+                        </div>
+                        </>
+                    )
+                })}
             </div>
         </div>
-        <div className='flex flex-col gap-3 w-full h-[20%]'>
-            <h1 className='font-semibold text-xl text-gray-400 cursor-pointer'>General</h1>
-            <div className='flex gap-4 items-center'>
-                <div className='text-2xl text-gray-400'><IoIosSettings/></div>
-                <Link href='/settings'><div className='text-gray-400 font-semibold cursor-pointer'>Settings</div></Link>
+        <div className='flex flex-col gap-3 w-full h-[30%]'>
+            <h1 className='font-semibold text-xl text-gray-400'>General</h1>
+            <div className='flex flex-col'>
+                {navlinksGeneral.map((links)=>{
+                    return (
+                        <>
+                        <div className='flex gap-4 items-center p-2 hover:bg-slate-200 w-[170px] rounded-lg'>
+                            <div className='text-2xl text-gray-400'>{links.icon}</div> 
+                            <Link href={`${links.href}`}><div className='text-gray-400 font-semibold cursor-pointer text-lg'>{links.name}</div></Link>
+                        </div>
+                        </>
+                    )
+                })}
             </div>
-            <div className='flex gap-4 items-center'>
-                {user ? 
-                <>
-                <div className='text-2xl text-gray-400'><FiLogOut/></div>
-                <button onClick={onLogout} className='text-gray-400 font-semibold cursor-pointer'>Logout</button>
-                </> :
-                <>
-                <div className='text-2xl text-gray-400'><FiLogIn/></div>
-                <button onClick={onLogIn} className='text-gray-400 font-semibold cursor-pointer'>LogIn</button>
-                </> }
+        </div>
+      </div>
+      <div className='px-5 flex flex-col'>
+        <div className='rounded-2xl flex justify-center items-center flex-col gap-3 py-3 bg-slate-100'>
+            <div className='text-5xl text-gray-400'><BsFillPersonFill/></div>
+            {user ? 
+            <>
+            <div className='text-gray-400 flex items-center gap-2 px-8 py-1 bg-slate-200 rounded-lg'>
+                <div className='text-3xl'><FiLogOut/></div>
+                <button onClick={onLogout} className='text-lg font-semibold'>Logout</button>
             </div>
+            </>
+            :
+            <>
+            <div className='text-gray-400 flex items-center gap-2 px-8 py-1 bg-slate-200 rounded-lg'>
+                <div className='text-3xl'><FiLogIn/></div>
+                <button onClick={onLogIn} className='text-lg font-semibold'>Login</button>
+            </div>
+            </>}
         </div>
       </div>
     </div>
